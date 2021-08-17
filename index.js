1b7e17e7ca6ca406ccfb4474b784197ce0e6b4e9
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-08-16 17:22:09 -0700

    lit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var cors = require('cors')
 var app = express()
-const PORT = process.env.PORT | 8081
+const PORT = 80
 app.use(cors())
 
 app.get('/', function (req, res, next) {
